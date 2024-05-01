782bf2a6756b16e4cbbf9ccc3870e8f94199a269
Ha Nguyen <hant582betterdev.contact@gmail.com>
2024-05-01 09:30:59 +0700

    Sort

--- api/v1/controller/task.controller.ts
+++ api/v1/controller/task.controller.ts
@@ -2,11 +2,29 @@ import { Request, Response } from "express";
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
+    // Find
+    interface Find {
+        deleted: boolean,
+        status?: string
+    }
     
-    const tasks = await Task.find({
-        deleted: false,
-        status: req.query.status
-    });
+    const find: Find = {
+        deleted: false
+    };
+
+    if(req.query.status) {
+        find.status = req.query.status.toString();
+    }
+    // End Find
+
+    // Sort
+    const sort = {};
+    if(req.query.sortKey && req.query.sortValue){
+        const sortKey = req.query.sortKey.toString();
+        sort[sortKey] = req.query.sortValue;
+    }
+    // End Sort
+    const tasks = await Task.find(find);
 
     res.json(tasks);
 }
