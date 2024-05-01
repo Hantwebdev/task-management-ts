49f8bb1ab568c4ef0fa5c1c5067cf27b8a0d12ef
Ha Nguyen <hant582betterdev.contact@gmail.com>
2024-05-01 13:14:47 +0700

    fixed

--- api/v1/routes/user.route.ts
+++ api/v1/routes/user.route.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 
 import * as controller from "../controller/user.controller";
 
+import * as authMiddleware from "../middleware/auth.middleware";
+
 const router: Router = Router();
 
 router.post('/register', controller.register);
 
 router.post('/login', controller.login);
 
-router.get('/detail/:id', controller.detail);
+router.get('/detail', authMiddleware.requireAuth, controller.detail);
 
-export const userRoutes: Router = router;
+export const userRoutes: Router = router; 
